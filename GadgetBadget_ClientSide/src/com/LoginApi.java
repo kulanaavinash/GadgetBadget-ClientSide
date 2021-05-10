8840e58653646c604ce321c59a3ae42377de584f
Kulana Avinash <kulanaavinash2018@gmail.com>
2021-05-11 03:43:45 +0530

    Authentication API Implemented

--- GadgetBadget_ClientSide/src/com/LoginApi.java
+++ GadgetBadget_ClientSide/src/com/LoginApi.java
@@ -1,5 +1,31 @@
 package com;
 
-public class LoginApi {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import logindao.Login;
+
+public class LoginApi extends HttpServlet {
+	private Login login = new Login();
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String uName = request.getParameter("uname");
+		String uPassword = request.getParameter("pass");
+
+		if (login.authentication(uName, uPassword)) {
+			HttpSession session = request.getSession();
+			session.setAttribute("username", uName);
+			response.sendRedirect("Index.jsp");
+		} else {
+			response.sendRedirect("Login.jsp");
+		}
+	}
 
 }
