f46b49a99fddd95ae5d6143658afda36fc1229c0
Kulana Avinash <kulanaavinash2018@gmail.com>
2021-05-10 21:44:23 +0530

    Prepared statement of Login class

--- GadgetBadget_ClientSide/src/logindao/Login.java
+++ GadgetBadget_ClientSide/src/logindao/Login.java
@@ -1,5 +1,34 @@
 package logindao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Login {
+	final String GET_ONE = "SELECT * FROM logins WHERE log_uname=? AND log_password=?";
+
+	private DBConnection connection = DBConnection.getInstance();
+
+	public boolean authentication(String userName, String password) {
+		PreparedStatement preparedStatement;
+		ResultSet resultSet;
+		try {
+			preparedStatement = connection.getConnection().prepareStatement(GET_ONE);
+			preparedStatement.setString(1, userName);
+			preparedStatement.setString(2, password);
+			resultSet = preparedStatement.executeQuery();
+			if (resultSet.next()) {
+				return true;
+			}
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		} finally {
+			connection.closeConnection();
+		}
+
+		return false;
+	}
 
 }
