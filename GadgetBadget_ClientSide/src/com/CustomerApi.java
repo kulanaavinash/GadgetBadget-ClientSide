09b5fe087518ac5a3bc39981af5b46cdd6c376c4
Kulana Avinash <kulanaavinash2018@gmail.com>
2021-05-11 14:59:36 +0530

    Customer Api

--- GadgetBadget_ClientSide/src/com/CustomerApi.java
+++ GadgetBadget_ClientSide/src/com/CustomerApi.java
@@ -1,5 +1,109 @@
 package com;
 
-public class CustomerApi {
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Customer;
+
+/**
+ * Servlet implementation class DoctorAPI
+ */
+@WebServlet("/CustomerApi")
+public class CustomerApi extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+
+	Customer customerObj = new Customer();
+
+	public CustomerApi() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String result = customerObj.insertCustomer(request.getParameter("customerName"),
+				request.getParameter("customerEmail"), request.getParameter("customerType"),
+				request.getParameter("customerContact"));
+
+		response.getWriter().write(result);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+
+	private Map<String, String> getParasMap(HttpServletRequest request) {
+		Map<String, String> map = new HashMap<String, String>();
+		try {
+			Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+			scanner.close();
+
+			String[] params = queryString.split("&");
+			for (String param : params) {
+				String[] p = param.split("=");
+				map.put(p[0], p[1]);
+			}
+
+		} catch (Exception e) {
+
+		}
+		return map;
+	}
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Map<String, String> param = getParasMap(request);
+
+		String result = customerObj.updateCustomer(param.get("hidcustomerIDSave").toString(),
+				param.get("customerName").toString().toString().replace("+", " "),
+				param.get("customerEmail").toString().toString().replace("%40", "@"),
+				param.get("customerType").toString().toString().replace("+", " "),
+				param.get("customerContact").toString());
+
+		response.getWriter().write(result);
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Map<String, String> param = getParasMap(request);
+
+		String result = customerObj.deleteCustomer(param.get("customerID").toString());
+
+		response.getWriter().write(result);
+	}
 
 }
